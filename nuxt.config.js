ac902359546e16bb5c108a3f799d952606df760e
Théotime Colin <contact@theotimecolin.com>
2020-10-19 08:44:13 +0200

    feat(Global): create boilerplate

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,37 +1,40 @@
+require('dotenv').config()
+
 export default {
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'instant-coffee',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+    head: {
+        title: 'instant-coffee',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+
+    css: [
+        '@/assets/scss/global.scss'
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+    plugins: [
+        { src: '@/plugins/base.js' }
+    ],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+    components: true,
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+    buildModules: [],
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+    modules: [
+        '@nuxtjs/axios'
+    ],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-  ],
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
+    build: {
+        extend (config) {
+            config.module.rules.push({
+                test: /\.svg.html$/,
+                loader: 'raw-loader'
+            })
+        }
+    }
 }
